0402be1178bb6cad7729aac3a03bc1df9080890e
mahipalsinghsodha <72567079+mahipalsinghsodha@users.noreply.github.com>
2023-11-14 13:04:41 +0530

    fist commit

---
+++ src/Components/Projects.jsx
@@ -0,0 +1,388 @@
+import React from 'react';
+import styled from 'styled-components';
+import kfc from '../Resources/Images/Project/ecommerce.png';
+import college from '../Resources/Images/Project/college.png';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import StreamIcon from '@mui/icons-material/Stream';
+import { useState } from 'react';
+
+const Projects = () => {
+    const [state, setState] = useState('')
+    const projects = [
+        {
+            id: 1,
+            title: 'Ecommerce Website',
+            img: kfc,
+            desc: 'Expert-led online learning for developers & IT professionals. Start learning today! On-demand training made simple. #1 training for individuals and businesses! Offline Access.',
+            worked: 'A collaborative project, built in 15 days by a itself.',
+            keyPoint1: 'Exercise with certified personal trainers whether you’re at home or on the road.',
+            keyPoint2: 'Professionally designed programs take the guesswork out of exercising.',
+            githubLink: 'https://github.com/mahipalsinghsodha/E-commerce-App',
+            deployedLink: 'https://calm-pink-bullfrog-ring.cyclic.cloud/',
+            tags: ['HTML', 'CSS', 'Javascript','React','Node']
+        },
+        {
+            id: 2,
+            title: 'College Website',
+            img: college,
+            desc: 'A college (Latin: collegium) is an educational institution or a constituent part of one. A college may be a degree-awarding tertiary educational institution, a part of a collegiate or federal university, an institution offering vocational education, a further education institution',
+            worked: 'A collaborative project, built in 5 days by a itself.',
+            keyPoint1: '',
+            keyPoint2: '',
+            githubLink: 'https://github.com/mahipalsinghsodha',
+            deployedLink: 'http://chandrawatittcollege.co.in/',
+            tags: ['HTML', 'CSS', 'Javascript']
+        },
+        
+    ]
+
+    return (
+        <Container id='projects' >
+            <h1>Projects</h1>
+            <div className='projectsContainer'>
+                {projects.map((ele, index) => (
+                    <div key={index} className="box">
+                        <div className='imgBox'>
+                            <img src={ele.img} alt={ele.title} width='100%' height="400px" />
+                        </div>
+                        <div className='ContentBox'>
+                            <h1>{ele.title}</h1>
+                            <p>{ele.desc}</p>
+                            <p style={{ fontWeight: 'bold' }}>{ele.worked}</p>
+                            <div className='tagsBox'>
+                                {ele.tags.map((tag, index) => (
+                                    <span key={index}>{tag}</span>
+                                ))}
+                            </div>
+                            <div className='linkSection'>
+                                <a href={ele.githubLink} target="_blank"><button><GitHubIcon />Github</button></a>
+                                <a href={ele.deployedLink} target="_blank"><button><StreamIcon />Live</button></a>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </Container >
+    )
+}
+
+
+const Container = styled.div`
+    width:100%;
+    margin:auto;
+    display:flex;
+    flex-direction:column;
+    gap:20px;
+    
+    h1{
+        color:#01a479;
+        text-transform:uppercase;
+    }
+
+    .projectsContainer{
+        width:80%;
+        margin:auto;
+        display:flex;
+        align-items:center;
+        flex-direction:column;
+    }
+
+    .box{
+        position:relative;
+        width:100%;
+        height:450px;
+        display:flex;
+        gap:10px;
+        margin-bottom:80px;
+        border: 10px solid #e9e9e9;
+        border-radius: 20px;
+        overflow:hidden;
+    }
+
+    .imgBox{
+        width:50%;
+        overflow:hidden;
+    }
+
+    
+    .imgBox>img{
+        display: block;
+        transition: transform .4s; 
+    }
+
+    .imgBox>img:hover{
+        position:absolute;
+        width:80%;
+        transform: scale(1.3);
+        transform-origin: 0% 0%;
+    }
+
+    
+    .ContentBox{
+        width:50%;
+        text-align:left;
+        padding:0px 20px 20px 20px;
+        
+    }
+
+    .ContentBox>h1{
+        color:#343a40;
+    }
+
+    .ContentBox>p{
+        width:90%;
+        font-size:large;
+        margin-top:0px;
+    }
+
+    .tagsBox{
+        width:80%;
+        display:flex;
+        align-items:center;
+        flex-wrap: wrap;
+        line-height:20px;
+        gap:5px;
+        margin-bottom:20px;
+    }
+
+    .tagsBox>span{
+        color:white;
+        background-color:#01a479;
+        padding: 2px 15px 2px 15px;
+        border-radius:10px;
+        font-size:13px;
+    }
+
+
+    .linkSection{
+        display:flex;
+        gap:20px;
+    }
+
+    .linkSection>a{
+        text-decoration:none;
+    }
+    .linkSection>a:first-child>button{
+        background-color:black;
+        color: White;
+        padding:8px 20px 8px 20px;
+        border-radius:8px;
+        font-weight:bold;
+        display:flex;
+        align-items:center;
+        justify-content:center;
+        gap:10px;
+        border:none;
+        cursor:pointer;
+    }
+
+    button{
+        cursor:pointer;
+    }
+
+    button:hover{
+        -webkit-transform: translateY(-2px);
+        transform: translateY(-2px);
+        -webkit-transition: all 0.5s;
+        transition: all 0.5s;
+    }
+    
+
+    .linkSection>a:last-child>button{
+        background-color:#44a4be;
+        color: White;
+        padding:8px 20px 8px 20px;
+        border-radius:8px;
+        border:none;
+        font-weight:bold;
+        display:flex;
+        align-items:center;
+        justify-content:center;
+        gap:10px;
+    }
+
+    @media only screen and (min-width: 768px) and (max-width:1120px){
+        .box{
+            flex-direction:column;
+            align-items:center;
+            height:100%;
+        }
+ 
+        .projectsContainer{
+            width:80%;
+            margin:auto;
+            
+        }
+        
+        .imgBox{
+            width:100%;
+            height:400px;
+        }
+
+        .imgBox>img{
+            width:100%;
+        }
+
+        .ContentBox{
+            width:80%;
+            display:flex;
+            align-items:center;
+            justify-content:center;
+            flex-direction:column;
+        }
+
+        .ContentBox>h1{
+            font-size:x-large;
+        }
+
+        .ContentBox>p{
+            font-size:15px;
+        }
+
+        .tagsBox{
+            width:100%;
+            justify-content:center;
+        }
+    }
+
+    @media only screen and (min-width: 481px) and (max-width:768px){
+        .box{
+           flex-direction:column;
+           align-items:center;
+           height:100%;
+        }
+
+        .projectsContainer{
+            width:80%;
+            margin:auto;
+            
+        }
+        
+        .imgBox{
+            width:100%;
+            height:280px;
+        }
+
+        .imgBox>img{
+            width:100%;
+        }
+
+        .ContentBox{
+            width:80%;
+            display:flex;
+            align-items:center;
+            justify-content:center;
+            flex-direction:column;
+            text-align:center;
+        }
+
+        .ContentBox>h1{
+            font-size:x-large;
+        }
+
+        .ContentBox>p{
+            font-size:15px;
+        }
+
+        .tagsBox{
+            width:100%;
+            justify-content:center;
+        }
+    }
+
+
+    @media only screen and (min-width:320px) and (max-width:480px){
+        .box{
+            flex-direction:column;
+            align-items:center;
+            height:100%;
+        }
+
+        .projectsContainer{
+            width:90%;
+            margin:auto;
+            
+        }
+
+        .imgBox{
+            width:100%;
+            height:200px;
+        }
+
+        .imgBox>img{
+            width:100%;
+        }
+
+        .ContentBox{
+            width:80%;
+            display:flex;
+            align-items:center;
+            justify-content:center;
+            flex-direction:column;
+            text-align:center;
+        }
+
+        .ContentBox>h1{
+            font-size:large;
+        }
+
+        .ContentBox>p{
+            font-size:13px;
+            
+        }
+
+
+        .tagsBox{
+            width:100%;
+            justify-content:center;
+        }
+    }
+
+    @media only screen and (max-width: 319px){
+        .box{
+            flex-direction:column;
+            align-items:center;
+            height:100%;
+        }
+
+        .projectsContainer{
+            width:90%;
+            margin:auto;
+            
+        }
+
+        .imgBox{
+            width:100%;
+            height:150px;
+        }
+
+        .imgBox>img{
+            width:100%;
+            
+        }
+
+        .ContentBox{
+            width:80%;
+            display:flex;
+            align-items:center;
+            justify-content:center;
+            flex-direction:column;
+            text-align:center;
+        }
+
+        .ContentBox>h1{
+            font-size:large;
+        }
+
+        .ContentBox>p{
+            font-size:13px;
+        }
+
+
+        .tagsBox{
+            width:100%;
+            justify-content:center;
+        }
+    }
+`
+export default Projects
